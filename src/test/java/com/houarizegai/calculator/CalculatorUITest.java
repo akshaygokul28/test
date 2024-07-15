cd23d4fcf02c4391667af79e16fb6b16d45b3c53
akshaygokul28 <114687408+akshaygokul28@users.noreply.github.com>
2024-07-15 15:57:42 +0530

    Update CalculatorUITest.java

--- src/test/java/com/houarizegai/calculator/CalculatorUITest.java
+++ src/test/java/com/houarizegai/calculator/CalculatorUITest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-new version
+
 class CalculatorUITest {
 
     private CalculatorUI calculatorUI;
